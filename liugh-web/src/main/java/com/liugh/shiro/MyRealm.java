40d4ba7c7be13a1951f32a84bff4ae3c10f9cb91
liugh <53182347@qq.com>
2020-06-03 20:36:20 +0800

    修改 认证问题

--- liugh-web/src/main/java/com/liugh/shiro/MyRealm.java
+++ liugh-web/src/main/java/com/liugh/shiro/MyRealm.java
@@ -109,6 +109,6 @@ public class MyRealm extends AuthorizingRealm {
         if (! JWTUtil.verify(token, userNo, userBean.getPassword())) {
             throw new AuthenticationException("Username or password error");
         }
-        return new SimpleAuthenticationInfo(userBean, token, this.getName());
+        return new SimpleAuthenticationInfo(token, token, this.getName());
     }
 }
